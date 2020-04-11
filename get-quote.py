30841690f025d4c658a77863e4751f531db10767
TIGirardi <55336837+TIGirardi@users.noreply.github.com>
2020-04-11 16:48:07 -0300

    Add the zen to quotes

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,39 @@
-import random
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse, io, random, sys
+
+# "import this" prints the zen to stdout at first import
+_, sys.stdout = sys.stdout, io.StringIO()
+import this
+_, sys.stdout = sys.stdout, _
+_.close()
+
+# def mk_parser():
+#   parser = argparse.ArgumentParser()
+#   parser.add_argument('number')
+
 
 def primary():
   with open("quotes.txt") as f:
-    quotes = f.readlines()
+    quotes = [line.strip() for line in f.readlines()]
 
-  rnd = random.randrange(len(quotes))
-  print('>', quotes[rnd], end='')
+  zen = this.s.translate(str.maketrans(this.d)) # The Zen of Python
+  zen = zen.splitlines()[2:] # strip the title
+  zen_special = '\n  > '.join(zen[7:9])
+  zen_errors = '\n  > '.join(zen[9:11])
+  zen_obvius = '\n  > '.join(zen[12:14])
+  zen_timing = '\n  > '.join(zen[14:16])
+
+  zen[14:16] = [zen_timing]
+  zen[12:14] = [zen_obvius]
+  zen[9:11] = [zen_errors]
+  zen[7:9] = [zen_special]
+
+  quotes += zen
+  n = len(quotes) - 1
+
+  rnd = random.randint(0, n)
+  print('>', quotes[rnd])
 
 if __name__== "__main__":
   primary()
